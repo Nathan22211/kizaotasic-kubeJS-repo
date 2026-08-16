3e50a5f8176cfea0bbb91e5e79e659dd5e83344b
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-08-16 10:54:03 -0500

    Update items.js

--- packs/kizaotastic/startup_scripts/items.js
+++ packs/kizaotastic/startup_scripts/items.js
@@ -1,3 +1,16 @@
 StartupEvents.registry('item', event => {
     event.create('kaolin_clay_ball')
+    
+    event.create('andesite_zinc_alloy')
+    event.create('andesite_iron_alloy')
+    event.create('granite_zinc_alloy')
+    event.create('granite_iron_alloy')
+    event.create('diorite_zinc_alloy')
+    event.create('diorite_iron_alloy')
+    //mekanism
+    event.create('alloy_starter')
+    event.create('basic_alloy')
+    //circuts
+    event.create('starter_control_circuit')
+    event.create('infused_control_circuit')
 })
