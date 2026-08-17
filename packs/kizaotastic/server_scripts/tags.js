05c10b83e8dae584d20a39efa4539762954ab1c1
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-08-17 09:35:28 -0500

    Update tags.js

--- packs/kizaotastic/server_scripts/tags.js
+++ packs/kizaotastic/server_scripts/tags.js
@@ -1,14 +1,17 @@
 ServerEvents.tags('item', event => {
-    // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+    // compatibility 
     event.add('minecraft:cobbles', 'nbtcompat:cobbled_stone')
     event.add('c:ingots/steel', 'progressionreforged:steel_ingot')
+    event.add('kubejs:gravels', 'nbtcompat:gravel')
+    event.add('kubejs:gravels', 'minecraft:gravel')
+    //alloys
     event.add('kubejs:zinc_alloys', 'kubejs:andesite_zinc_alloy')
     event.add('kubejs:zinc_alloys', 'kubejs:granite_zinc_alloy')
     event.add('kubejs:zinc_alloys', 'kubejs:diorite_zinc_alloy')
     event.add('kubejs:iron_alloys', 'kubejs:andesite_iron_alloy')
     event.add('kubejs:iron_alloys', 'kubejs:granite_iron_alloy')
     event.add('kubejs:iron_alloys', 'kubejs:diorite_iron_alloy')
-
+    //casings
     event.add('kubejs:zinc_casings', 'custom_tiers:andesite_zinc_casing')
     event.add('kubejs:zinc_casings', 'custom_tiers:granite_zinc_casing')
     event.add('kubejs:zinc_casings', 'custom_tiers:diorite_zinc_casing')
