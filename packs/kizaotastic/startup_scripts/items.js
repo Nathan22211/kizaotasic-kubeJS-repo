6f5f1e05fd71fb70ec84707e663855ec8440cef9
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-08-18 12:01:10 -0500

    Update items.js

--- packs/kizaotastic/startup_scripts/items.js
+++ packs/kizaotastic/startup_scripts/items.js
@@ -1,15 +1,15 @@
 StartupEvents.registry('item', event => {
     event.create('kaolin_clay_ball')
     
-    event.create('andesite_zinc_alloy')
-    event.create('andesite_iron_alloy')
-    event.create('granite_zinc_alloy')
-    event.create('granite_iron_alloy')
-    event.create('diorite_zinc_alloy')
-    event.create('diorite_iron_alloy')
+    event.create('andesite_zinc_alloy').texture('kubejs:item/andesite-alloy')
+    event.create('andesite_iron_alloy').texture('kubejs:item/andesite-alloy')
+    event.create('granite_zinc_alloy').texture('kubejs:item/granite-alloy')
+    event.create('granite_iron_alloy').texture('kubejs:item/granite-alloy')
+    event.create('diorite_zinc_alloy').texture('kubejs:item/diorite-alloy')
+    event.create('diorite_iron_alloy').texture('kubejs:item/diorite-alloy')
     //mekanism
     event.create('alloy_starter').texture('kubejs:item/starter-alloy')
-    event.create('basic_alloy')
+    event.create('basic_alloy').texture('kubejs:item/basic-alloy')
     //circuts
     event.create('starter_control_circuit').texture('kubejs:item/starter-control-circuit')
     event.create('infused_control_circuit').texture('kubejs:item/infused-control-circuit')
