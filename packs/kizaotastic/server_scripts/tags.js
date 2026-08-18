e9a4378f6895a653997ab9857058105c1b5ff2af
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-08-18 16:43:31 -0500

    Update tags.js

--- packs/kizaotastic/server_scripts/tags.js
+++ packs/kizaotastic/server_scripts/tags.js
@@ -11,6 +11,13 @@ ServerEvents.tags('item', event => {
     event.add('kubejs:iron_alloys', 'kubejs:andesite_iron_alloy')
     event.add('kubejs:iron_alloys', 'kubejs:granite_iron_alloy')
     event.add('kubejs:iron_alloys', 'kubejs:diorite_iron_alloy')
+    //shafts
+    event.add('kubejs:zinc_alloy_shafts', 'create_kinetics_tiers:andesite_zinc_alloy_shaft')
+    event.add('kubejs:zinc_alloy_shafts', 'create_kinetics_tiers:granite_zinc_alloy_shaft')
+    event.add('kubejs:zinc_alloy_shafts', 'create_kinetics_tiers:diorite_zinc_alloy_shaft')
+    event.add('kubejs:iron_alloy_shafts', 'create_kinetics_tiers:andesite_iron_alloy_shaft')
+    event.add('kubejs:iron_alloy_shafts', 'create_kinetics_tiers:granite_iron_alloy_shaft')
+    event.add('kubejs:iron_alloy_shafts', 'create_kinetics_tiers:diorite_iron_alloy_shaft')
     //casings
     event.add('kubejs:zinc_casings', 'custom_tiers:andesite_zinc_casing')
     event.add('kubejs:zinc_casings', 'custom_tiers:granite_zinc_casing')
