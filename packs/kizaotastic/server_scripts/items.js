dee1b4bd7624dc62f97ab1e1e4a4909375ea1e59
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-08-19 11:31:58 -0500

    Update items.js

--- packs/kizaotastic/server_scripts/items.js
+++ packs/kizaotastic/server_scripts/items.js
@@ -19,6 +19,10 @@ ServerEvents.recipes(event => {
   event.recipes.mekanism.metallurgic_infusing("kubejs:starter_control_circuit", "minecraft:copper_ingot", "10x mekanism:redstone", false)
   event.remove({ output: 'mekanism:alloy_infused' })
   event.recipes.mekanism.metallurgic_infusing("kubejs:alloy_starter", "minecraft:copper_block", "10x mekanism:redstone", false)
+
+  event.recipes.create.mixing(['#kubejs:iron_alloys', 'minecraft:redstone_dust'], ['kubejs:powdered_infused_alloy'])
+  event.recipes.mekanism.enriching('mekanism:alloy_infused', 'kubejs:powdered_infused_alloy')
+  
   //basic items
   event.shapeless(
     Item.of('kubejs:kaolin_clay_ball', 1), // arg 1: output
